bc6aaa2e5e42207337cbb4dedb0134c965352854
Abdiel Cortes <zurken@fb.com>
2020-06-27 17:48:22 -0700

    created PhotoViewController with table view

---
+++ Tumblr/ViewControllers/PhotosViewController.h
@@ -0,0 +1,17 @@
+//
+//  PhotosViewController.h
+//  Tumblr
+//
+//  Created by zurken on 6/27/20.
+//  Copyright © 2020 FacebookUniversity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotosViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tumblr/ViewControllers/PhotosViewController.m
@@ -0,0 +1,72 @@
+//
+//  PhotosViewController.m
+//  Tumblr
+//
+//  Created by zurken on 6/27/20.
+//  Copyright © 2020 FacebookUniversity. All rights reserved.
+//
+
+#import "PhotosViewController.h"
+
+@interface PhotosViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (strong, nonatomic) NSArray *posts;
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation PhotosViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    
+    [self fetchPosts];
+}
+
+-(void) fetchPosts {
+    NSURL *url = [NSURL URLWithString:@"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            if (error != nil) {
+                NSLog(@"%@", [error localizedDescription]);
+            }
+            else {
+                NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+//                NSLog(@"%@", dataDictionary);
+
+                self.posts = dataDictionary[@"response"][@"posts"];
+                NSLog(@"%@", self.posts);
+                
+                // TODO: Get the posts and store in posts property
+                // TODO: Reload the table view
+            }
+        }];
+    [task resume];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.posts.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc] init];
+    
+    return cell;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
